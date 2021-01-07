b794bf6bbc2af980df26abdd9d327285e648f618
Malte B.P <maltebp94@gmail.com>
2021-01-07 13:26:15 +0100

    Added ECS

---
+++ CaptainCombat/Source/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CaptainCombat
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+
+            Domain domain = new Domain();
+
+            Entity entity = new Entity(domain);
+            var c = entity.AddComponent<Transform>("Hello world");
+
+            var entity2 = new Entity(domain);
+            entity2.AddComponent<Transform>("Entity 2s");
+            entity2.AddComponent<TestComponent2>().s = "Entity 2";
+
+            // TypeList.add<TestComponent>().add<TestComponent>() 
+            //domain.forMatchingEntities(new Type[]{ typeof(TestComponent)});
+
+            domain.Clean();
+
+            Console.WriteLine();
+
+            domain.ForMatchingEntities<Transform>((e) => {
+                Console.WriteLine(e.GetComponent<Transform>().s);
+                e.Delete();
+            });
+
+            Console.WriteLine();
+
+            Console.WriteLine("Final: ");
+            //domain.clean();
+            domain.ForMatchingEntities<Transform>((e) => {
+                Console.WriteLine(e.GetComponent<Transform>().s);
+            });
+
+            Console.WriteLine("\nDone!");
+
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
---
+++ CaptainCombat/Source/Transform.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static ECS.Domain;
+
+namespace PirateCombatTemp {
+
+    class Transform : Component {
+
+        public string s;
+
+        public Transform(Entity e, string s) : base(e) {
+            this.s = s;
+        }
+
+    }
+
+
+
+
+
+    class TestComponent2 : Component {
+
+        public string s;
+
+        public TestComponent2(Entity e) : base(e) {
+
+        }
+
+    }
+}
