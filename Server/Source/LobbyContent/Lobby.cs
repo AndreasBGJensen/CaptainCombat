7b73dc1b135c09cc992af5bf7eb818977538458f
AndreasBGJensen <bgandreas@hotmail.com>
2021-01-18 15:05:34 +0100

    Implementet some server logic

---
+++ Server/Source/LobbyContent/Lobby.cs
@@ -0,0 +1,63 @@
+﻿using CaptainCombat.Common;
+using dotSpace.Interfaces.Space;
+using dotSpace.Objects.Network;
+using dotSpace.Objects.Space;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tuple = dotSpace.Objects.Space.Tuple;
+
+namespace CaptainCombat.Server.Source.LobbyContent
+{
+    public class Lobby
+    {
+
+        private SpaceRepository repository;
+        private SequentialSpace lobby;
+        public string lobbyUrl { get; set; }
+        public string spaceID { get; set; }
+        public string creator { get; set; }
+        public Lobby(SpaceRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public ITuple CreateLobby(string username, int user_id)
+        {
+            creator = username;
+            CreateUri(user_id);
+            if(lobbyUrl != null)
+            {
+                AddGameLobby();
+                
+                return new Tuple("lobbyCreationResponse", user_id, username, lobbyUrl);
+            }
+            return new Tuple("lobbyCreationResponse", user_id, username, "You have alreadey created a lobby");
+
+        }
+
+        private void CreateUri(int user_id)
+        {
+            spaceID = "game" + user_id;
+           lobbyUrl = "tcp://" + ConnectionInfo.SERVER_ADDRESS + "/"+ spaceID+ "?KEEP";
+            
+            if (!LobbyBookKeeping.AddLobby(this))
+            {
+                throw new ArgumentException("Lobby Could not be saved in database");
+            }
+            
+            
+        }
+
+
+        private void AddGameLobby()
+        {
+            lobby = new SequentialSpace();
+            repository.AddSpace(spaceID, lobby);
+
+        }
+
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,64 @@
+﻿using CaptainCombat.Common;
+using CaptainCombat.Common.Singletons;
+using dotSpace.Interfaces.Space;
+using dotSpace.Objects.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string serverUrl = "tcp://" + ConnectionInfo.SERVER_ADDRESS + "/" + ConnectionInfo.SPACE_NAME + "?KEEP";
+            Console.WriteLine($"Connecting to server at '{serverUrl}'...");
+
+            ISpace space = new RemoteSpace(serverUrl);
+
+            try
+            {
+                // Connection is made first time some request is being made
+                space.QueryP("disregard this string content");
+            }
+            catch (SocketException e)
+            {
+                // TODO: Handle connection issues here
+                Console.WriteLine("Connection to server failed\n");
+                throw e;
+            }
+
+            Connection connecting = Connection.Instance;
+            connecting.Space = space;
+
+            Console.WriteLine("Connection to server succeeded\n");
+
+
+            connecting.Space.Put("createLobby", "Andreas", 1);
+            connecting.Space.Put("createLobby", "Per", 2);
+            connecting.Space.Put("createLobby", "Hans",3);
+
+            ITuple respons = connecting.Space.Get("lobbyResponse", 1, "Andreas", typeof(string));
+            GetAllLobbys(connecting.Space);
+
+
+            Console.ReadLine();
+
+        }
+
+        static void GetAllLobbys(ISpace space)
+        {
+            IEnumerable<ITuple> allLobbyes = space.QueryAll("createdLubby", typeof(string), typeof(string), typeof(string));
+            foreach(ITuple x in allLobbyes)
+            {
+                Console.WriteLine((string)x[1] + (string)x[2], (string)x[3]);
+            }
+
+
+        }
+    }
+}
